db3a9167d982d587da6c24be906371c75dab2c4c
Songj2 <55727461+Songj2@users.noreply.github.com>
2021-09-30 11:20:56 +0900

    Update Greedy_MultiplyPlus.py
    # 큰 수가 만들어질 조건 수정

--- Greedy_MultiplyPlus.py
+++ Greedy_MultiplyPlus.py
@@ -4,7 +4,7 @@ result= 0
 
 for i in sNum: # 한자리씩 읽어오기
     i= int(i) # int로 형변환
-    if i > 2 and result != 0: #큰수가 만들어질 조건
+    if i > 1 and result > 1: #큰수가 만들어질 조건 **기존: if i > 2 and result != 0:
         result*= i
 
     else:
