02addcbbf77402c5a7e81b34a124209abe807c82
Songj2 <55727461+Songj2@users.noreply.github.com>
2021-09-30 10:54:54 +0900

    2021.09.30
    더하기 혹은 곱하기를 이용하여 큰 수 만들기

---
+++ Greedy_MultiplyPlus.py
@@ -0,0 +1,13 @@
+# 곱하기 혹은 더하기
+sNum= input()
+result= 0
+
+for i in sNum: # 한자리씩 읽어오기
+    i= int(i) # int로 형변환
+    if i > 2 and result != 0: #큰수가 만들어질 조건
+        result*= i
+
+    else:
+        result+= i
+
+print(result)
