8d14171d7ff480c042389748aa0bc58e1d51f93e
Songj2 <55727461+Songj2@users.noreply.github.com>
2021-10-08 19:18:19 +0900

    2021.10.08
    체육복 빌려주기
    # 50점짜리 답안

--- Greedy_GymSuit.py
+++ Greedy_GymSuit.py
@@ -1,38 +1,29 @@
 def solution(n, lost, reserve):
-    answer = n- len(lost)
     inNum= 0
     check= [1]* n
     for j in lost:
-        check[j-1]= 0
+        check[j-1]= -1
     
 
     while inNum<len(lost):
+        loTemp= lost[inNum]- 1
+        
         for i in range(len(reserve)):
             temp= reserve[i]- 1
-            if check[temp]== 1:
-                print(check, lost[inNum], reserve[i])
-                if lost[inNum]== reserve[i]:
-                    answer+=1
-                    check[temp]= 0
-                    print("==")
-                    break
-                    
-                elif lost[inNum]== reserve[i]-1:
-                    answer+=1
-                    check[temp]= 0
-                    print("-1")
-                    break
-                    
-                elif lost[inNum]== reserve[i]+1:
-                    answer+=1
-                    check[temp]= 0
-                    print("+1")
-                    break
             
+            if check[temp]== 1 and check[loTemp]== -1:
+                if lost[inNum]== reserve[i]- 1 or lost[inNum]== reserve[i] or lost[inNum]== reserve[i]+ 1:
+                    check[temp]= 0
+                    check[loTemp]= 0
+                    break
+                    
         inNum+=1
+        
                 
             
-    return answer, check
+    return n- check.count(-1)
+
+
 
 #전체 학생수: n, 도난당한 학생 리스트: lost, 여분 체육복 학생 리스트: reserve
 
